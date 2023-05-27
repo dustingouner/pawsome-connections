53764f5324f9a130e16dafde44aec0d9fd9df6b2
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-27 13:19:58 -0700

    feat: fetch data from petfinder API

--- src/Api-Calls.tsx
+++ src/Api-Calls.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 
 const getAnimals = () => {
-     return (fetch(
+     return fetch(
        "https://api.petfinder.com/v2/animals?location=60521&type=dog&age=senior",
        {
          method: "GET",
          headers: {
            Authorization:
-             "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJvMzRDd0g2anF3QWdMQ2JZY2VjNHQwWlJSeFdTckwwR3lFQm5OM3dSVUxVdGFNMWI0QiIsImp0aSI6IjcyNDRhN2ZiMmI3N2FlMGZhMDdmM2Y4ZWM3YTlhN2U4ZTJkOWJkNWU2ZTMzNDc4ZWU5NzE2NWVmNDgyNTYxNDE4M2UxMWY0NTdlMWQ5NmM2IiwiaWF0IjoxNjg1MjEyNDA0LCJuYmYiOjE2ODUyMTI0MDQsImV4cCI6MTY4NTIxNjAwNCwic3ViIjoiIiwic2NvcGVzIjpbXX0.z1Kb6mHnpHU0K5YmB0RKzPshPqu79w4jwU1vNGVqqzCbBk__mskqM1GA52LjTBVyizrlTAkaUttXtYkgfyyIPhwfSNIodcUHfz33Y9FCUutHdmo-dtEsCHt28yB_Zi-1QsKmhiGRg1zsJRKBCHP6TEjQzYUAAapU6_udKQVNLXPOk0Z0kN5eU1zVTpDITfQk1eu5kMZY_qmYM7SmT9mFKBZMBv2CMzth0tptZfuJWKr1sgMEmGNtYKktgYXBjT5H7gX9qF-crvrydcwLDlnFm1zvdmRERJCXvMonOixzpPDWlX7MuDazGt_-190q3PJE2EXKoofgaGFiZ9L1s6IVkw",
+             "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJvMzRDd0g2anF3QWdMQ2JZY2VjNHQwWlJSeFdTckwwR3lFQm5OM3dSVUxVdGFNMWI0QiIsImp0aSI6IjIyYzZjNjE5ZjJkMTI2MjQ2NDdlMDY2NjQ1ZTE1NGU0ZDg0N2M2YTU4NzdhNzM2NjY0NmQwYzlhYjUxYjQzNTMxNTkwYjAyODQ4M2E3MDdhIiwiaWF0IjoxNjg1MjE1OTYwLCJuYmYiOjE2ODUyMTU5NjAsImV4cCI6MTY4NTIxOTU2MCwic3ViIjoiIiwic2NvcGVzIjpbXX0.OdFVYiE6wVZ9dl0QUQdnVykSwB3Q1Tflo1ENuaxyamHmHaKCuQExesPuyZQSzYvJZxyzK6-8-8Z6yZz1-87SysnBQZMJogKoJxLybjydHjlWXQxmYu13MQRUEwmTc_Nhg9cNlXZpQHWv4Nuhp_a-3zoQ68qJSHZ7AR8PMqrlhunkBAYQgiJMtCgn_P4jtn6lfPw4iva_4KNowDGAOC0nFZJ_hvOtoEXm8gTTPANOhrDNwssG3kQSj-cFfQDS4vgA4pWpNb04rHANMLmYq58THJLJ9fz9RWcrV8fIDz-GqYhRvjlbtJc1q73Iibz63PPfbQkSU7hDqZnYuesZDrUEYA",
          },
        }
-     )
-       .then((response) => response.json())
-    //    .then((data) => {
-    //      // Handle the response data
-    //      console.log(data);
-    //    })
-    //    .catch((error) => {
-    //      // Handle any errors
-    //      console.error("Error:", error);
-    //    });
-     )
+     ).then((response) => response.json());
+     //    .then((data) => {
+     //      // Handle the response data
+     //      console.log(data);
+     //    })
+     //    .catch((error) => {
+     //      // Handle any errors
+     //      console.error("Error:", error);
+     //    });
 }
 
 export default getAnimals;
