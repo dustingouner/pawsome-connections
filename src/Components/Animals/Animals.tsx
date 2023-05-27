8450aae80f084550163b8a546e4ad237bad71de9
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-27 13:24:23 -0700

    feat: add interfact to animal props

--- src/Components/Animals/Animals.tsx
+++ src/Components/Animals/Animals.tsx
@@ -1,6 +1,23 @@
 import React from "react";
+type Animal = {
+  id: number;
+  url: string;
+  // "age":string,
+  // breeds: object,
+  // contact:object,
+  // description:string,
+  // gender:string,
+  // name:string,
+  // size: string,
+  // species: string,
+  // type: string,
+};
 
-const Animals = () => {
+interface AnimalProps {
+  animals:Animal[]
+}
+
+const Animals:React.FC<AnimalProps> = (props:AnimalProps) => {
   return (
     <section className="animals-section">
      
