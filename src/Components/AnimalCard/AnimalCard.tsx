968db91f5b199b00ae33dc0967035a1e0ebfba0b
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-29 16:41:28 -0700

    refact: naming for css

--- src/Components/AnimalCard/AnimalCard.tsx
+++ src/Components/AnimalCard/AnimalCard.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import './AnimalCard.css'
 
-
 type Animal = {
   id: number;
   primary_photo_cropped: {
@@ -44,14 +43,16 @@ interface AnimalProps {
 
 const AnimalCard:React.FC<AnimalProps> = (props:AnimalProps) => {
   const { animalDetails } = props
-  const fallBackImage = require('../../assets/pawsome.png')  
+  const fallBackImage = require('../../assets/sorry-image.png')  
+  const heart = require('../../assets/heart.png')  
   const imgSrc = animalDetails.primary_photo_cropped?.small
 
   return (
     <section className="animal-card">
         <img className="animal-img" src={imgSrc || fallBackImage} alt="Photos are in the works!" />
+        <img className="heart-icon" src={heart}></img>
         <h2 className="animal-name">{animalDetails.name}</h2>
-        <p>{`${animalDetails.age} | ${animalDetails.breeds.primary} | ${animalDetails.contact.address.city}, ${animalDetails.contact.address.state}`}</p>
+        <p className="animal-details">{`${animalDetails.age} | ${animalDetails.breeds.primary} | ${animalDetails.contact.address.city}, ${animalDetails.contact.address.state}`}</p>
     </section>
   );
 }
