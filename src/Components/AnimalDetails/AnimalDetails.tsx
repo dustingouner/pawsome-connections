e73d727b47a7739619b45da7d9206717bee22075
Dustin Gouner <dustingouner20@gmail.com>
2023-05-29 00:26:24 -0600

    feat: render selected pet details to the animal details page

--- src/Components/AnimalDetails/AnimalDetails.tsx
+++ src/Components/AnimalDetails/AnimalDetails.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useParams } from "react-router-dom"
+import './AnimalDetails.css'
 
 type Animal = {
   id: number;
@@ -34,6 +36,12 @@ type Animal = {
   size: string,
   species: string,
   type: string,
+  status: string,
+  attributes: {
+    house_trained: boolean,
+    spayed_neutered: boolean,
+    shots_current: boolean,
+  }
 };
 
 interface AnimalProps {
@@ -41,24 +49,42 @@ interface AnimalProps {
 }
 
 const AnimalDetails:React.FC<AnimalProps> = (props:AnimalProps) => {
+  const { animals } = props
+  const { id } = useParams<{id: string }>()
+
+  const selectedAnimal = animals.find(animal => animal.id === Number(id))
+
+  if (!selectedAnimal) {
+    return <p>We're sorry, we could not locate this pet.</p>
+  }
+
+  const { primary_photo_cropped, age, status, breeds, contact, description, gender, name, size, type, attributes} = selectedAnimal
+  const imgSrc = primary_photo_cropped?.small
+  const fallBackImage = require('../../assets/pawsome.png')
+  console.log('petDetails', animals)
   return (
-    <div>
-      <h1>Pet Name</h1>
-      <div>
-        <img/>
+    <div className="animal-details-container">
+        <h1 className="pet-name">{name}</h1>
+      <div className="animal-details">
+        <div className="image-container">
+           <img className="pet-photo" src={imgSrc || fallBackImage} alt={`${name}'s photo` || 'Pet photo not available, logo image shown'}/>
+        </div>
         <section className="animal-info">
-          <p>Adoptable:</p>
-          <p>Gender:</p>
-          <p>Breed:</p>
-          <p>Color:</p>
-          <p>Coat:</p>
-          <p>Spayed/Neutered:</p>
-          <p>House Trained:</p>
-          <p>Description:</p>
+          <p className="description">Description: {description}</p>
+          <p>Adoption Status: {status} </p>
+          <p>Species: {type}</p>
+          <p>Gender: {gender}</p>
+          <p>Breed: {breeds.primary}</p>
+          <p>Age: {age} </p>
+          <p>Size: {size}</p>
+          <p>Spayed/Neutered: {attributes.spayed_neutered && 'yes' || 'no'}</p>
+          <p>House Trained: {attributes.house_trained && 'yes' || 'no'}</p>
+          <p>Spayed/Neutered: {attributes.spayed_neutered && 'yes' || 'no'}</p>
         </section>
         <section className="contact-info">
-          <p>Email:</p>
-          <p>Phone Number:</p>
+          <p>Location: {contact.address.city}, {contact.address.state}</p>
+          <p>Email: {contact.email}</p>
+          <p>Phone Number: {contact.phone}</p>
         </section>
       </div>
     </div>
--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -43,6 +43,12 @@ type Animal = {
   size: string,
   species: string,
   type: string,
+  status: string,
+  attributes: {
+    house_trained: boolean,
+    spayed_neutered: boolean,
+    shots_current: boolean,
+  }
 };
  
 // async function App() {
@@ -89,7 +95,7 @@ type Animal = {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/:id" >
+        <Route exact path="/:id" component  ={AnimalDetails}>
           <Header />
           <AnimalDetails animals={animals}/>
         </Route>
