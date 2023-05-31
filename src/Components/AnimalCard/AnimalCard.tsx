48ca7d748070a3d0b30d4dae785eca0973d17ada
Matt Saqueli <msaqueli22@turing.edu>
2023-05-31 15:34:57 -0500

    Add favoriting emoji, changes on click

--- src/Components/AnimalCard/AnimalCard.tsx
+++ src/Components/AnimalCard/AnimalCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './AnimalCard.css'
 import { Animal } from "../../types";
 
@@ -10,13 +10,22 @@ interface AnimalProps {
 const AnimalCard:React.FC<AnimalProps> = (props:AnimalProps) => {
   const { animalDetails } = props
   const fallBackImage = require('../../assets/sorry-image.png')  
-  const heart = require('../../assets/heart.png')  
+  // const heart = require('../../assets/heart.png')  
   const imgSrc = animalDetails.primary_photo_cropped?.small
+  const [isFavorite, setFavorite] = useState(false)
+
+  const toggleFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    setFavorite(!isFavorite)
+  }
 
   return (
     <section className="animal-card">
         <img className="animal-img" src={imgSrc || fallBackImage} alt="Photos are in the works!" />
-        <img className="heart-icon" src={heart}></img>
+        {/* <img className="heart-icon" src={heart}></img> */}
+        <button className="favorite-button" onClick={toggleFavorite} >
+        {isFavorite ? "🩷" : "🤍"}
+      </button>
         <h2 className="animal-name">{animalDetails.name}</h2>
         <p className="animal-card-details">{`${animalDetails.age} | ${animalDetails.breeds.primary} | ${animalDetails.contact.address.city}, ${animalDetails.contact.address.state}`}</p>
     </section>
