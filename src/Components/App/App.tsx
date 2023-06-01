4bf33f4c0776fb5d8579d6c7a5fa50b74c428047
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-31 21:59:19 -0700

    feat: add state to animal type, call method in form to update state

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -4,7 +4,7 @@ import Header from '../Header/Header';
 import Form from '../Form/Form';
 import Animals from '../Animals/Animals';
 import AnimalDetails from '../AnimalDetails/AnimalDetails'
-import { getAnimals, getLocation } from '../../Api-Calls';
+import { getAnimals, getLocation, getType } from '../../Api-Calls';
 import {useState, useEffect} from 'react'
 import { Route, Switch } from 'react-router-dom';
 import { Animal, EventHandler } from '../../types';
@@ -15,27 +15,50 @@ import { Animal, EventHandler } from '../../types';
     const [location, setLocation] = useState<string>('');
     const [favorites, setFavorites] = useState<Animal[]>([]);
     const [button, setButton] = useState<string>('Show Favorites');
+    const [animalType, setAnimal] = useState<string>('')
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          let data;
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     try {
+    //       let data;
           
-          if(location !== "") {
-            data = await getLocation(location)
-          } else {
-            data = await getAnimals()
-          }
+    //       if(location !== "") {
+    //         data = await getLocation(location)
+    //       } else {
+    //         data = await getAnimals()
+    //         console.log(data, "data")
+    //       }
         
-          setAnimals(data.animals);
+    //       setAnimals(data.animals);
 
-          } catch (error) {
-            setError(error);
-          }
-      };
+    //       } catch (error) {
+    //         setError(error);
+    //       }
+    //   };
   
-      fetchData();
-    }, [location]);
+    //   fetchData();
+    // }, [location]);
+
+
+     useEffect(() => {
+       const fetchData = async () => {
+         try {
+           let data;
+           if (animalType !== "all") {
+             data = await getType(animalType);
+           } else if (location !== "") {
+             data = await getLocation(location);
+           } else {
+             data = await getAnimals();
+           }
+           setAnimals(data.animals);
+         } catch (error) {
+           setError(error);
+         }
+       };
+
+       fetchData();
+     }, [animalType, location]);
 
 
     const favoriteAnimals:Function = (id:number) => {
@@ -56,17 +79,12 @@ import { Animal, EventHandler } from '../../types';
       if(favorites && favorites !== animals) {
         setAnimals(favorites)
         setButton("Show All")
-        // document.getElementById('showFavorites').innerText = 'Show All'
       } else if(favorites ) {
         getAnimals()
         .then(data => setAnimals(data.animals))
         setButton("Show Favorites")
       }
     }
-
-  console.log(favorites, 'favoriteseeeese')
-  console.log(location, "line 60")
-  console.log(animals, "animals");
   
   return (
     <div className="App">
@@ -79,7 +97,7 @@ import { Animal, EventHandler } from '../../types';
           <Header />
           <div className="submission-container">
           <button id="showFavorites" className='show-favorites-btn' onClick={event => displayFavorites(event)}>{button}</button>
-          <Form setLocation={setLocation} />
+          <Form setLocation={setLocation} setAnimal={setAnimal} />
           </div>
           <Animals animals={animals} favoriteAnimals={favoriteAnimals} unfavoriteAnimals={unfavoriteAnimals}/>
         </Route>
