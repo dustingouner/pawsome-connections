76942f8b5efe7df1ae62396a2df71bd19063cfa2
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-03 18:21:20 -0700

    testing: add testing to form and main page for displaying cards and clicking on card details

--- cypress/e2e/dashboard_spec.cy.ts
+++ cypress/e2e/dashboard_spec.cy.ts
@@ -13,7 +13,7 @@ describe('Dashboard', () => {
   it("should display a button to take the user back home ", () => {
     cy.get(".show-favorites-btn ").should("have.text", "Show Favorite Pets")
   })
-  it("should see a form with the option to select by pet type, filer by zipcode, and submit form", () => {
+  it("should see a navigation section with the option to select by pet type, filter by zipcode, and submit the search", () => {
     cy.get("form")
       .get("select")
       .first()
@@ -24,22 +24,26 @@ describe('Dashboard', () => {
       .get("input")
       .first()
       .should("have.attr", "placeholder", "Input Zip Code")
-      .get('input[name="submitForm"]')
+      .get('input[type="submit"]')
+      .should("be.visible")
   })
   it("should have a list of cards with animals to adopt", () => {
-    cy.get(".animals-container").find(".animal-card").should("have.length", 4)
+    cy.get(".animals-container").find(".animal-card ").should("have.length", 4);
+    cy.get(".animals-container > :nth-child(1)").contains("Sadie");
+    cy.get(".animals-container > :nth-child(2)").contains("Gambit");
+    cy.get(".animals-container > :nth-child(3)").contains("Dexter");
   })
+  it("should allow a user to click on an animal card and navigate to the animals detail page", () => {
+    cy.get(".animal-card").first().click();
+    cy.url().should("include", "/animal/64841660")
+  })
+})
   it("should see a error in the case of a bad GET response", () => {
     cy.intercept("https://api.petfinder.com/v2/animals?age=senior", {
       statusCode: 404,
     }).visit("http://localhost:3000");
     cy.get(".header-section").get("p").should("have.text", "Failed to Fetch");
   });
-  it("should allow a user to click on an animal card and navigate to the animals detail page", () => {
-    cy.get(".animal-card").first().click()
-    cy.url().should("include", "/animal/64841660")
-  })
-})
 describe("Search Form", () => {
   beforeEach("search input", () => {
      cy.intercept("GET",'https://api.petfinder.com/v2/animals?age=senior', {
