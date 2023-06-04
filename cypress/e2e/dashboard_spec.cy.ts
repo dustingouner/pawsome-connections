e98d1c01549bda3c410400e68d30cc46383ee460
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-06-03 17:13:14 -0700

    add cypress files

---
+++ cypress/e2e/dashboard_spec.cy.ts
@@ -0,0 +1,65 @@
+///<reference types="Cypress" />
+
+describe('Dashboard', () => {
+  beforeEach('Visit homepage', () => {
+    cy.intercept("GET",'https://api.petfinder.com/v2/animals?age=senior', {
+      fixture:'animalsData.json'
+    })
+      .visit('http://localhost:3000/');
+  });
+  it("has a header with an image of the logo and title of website", () => {
+    cy.get("header").should("is.visible").find("img").should("be.visible");
+  });
+  it("should display a button to take the user back home ", () => {
+    cy.get(".show-favorites-btn ").should("have.text", "Show Favorite Pets")
+  })
+  it("should see a form with the option to select by pet type, filer by zipcode, and submit form", () => {
+    cy.get("form")
+      .get("select")
+      .first()
+      .contains("All Pets")
+      .get("select")
+      .last()
+      .contains("Cat")
+      .get("input")
+      .first()
+      .should("have.attr", "placeholder", "Input Zip Code")
+      .get('input[name="submitForm"]')
+  })
+  it("should have a list of cards with animals to adopt", () => {
+    cy.get(".animals-container").find(".animal-card").should("have.length", 4)
+  })
+})
+describe("Search Form", () => {
+  beforeEach("search input", () => {
+     cy.intercept("GET",'https://api.petfinder.com/v2/animals?age=senior', {
+      fixture:'animalsData.json'
+    })
+    cy.intercept(
+      "GET",
+      "https://api.petfinder.com/v2/animals?age=senior&location=31601",
+      {
+        fixture: "animalsData.json",
+      }
+    );
+    cy.visit("http://localhost:3000/");
+  });
+  it("should be able to input data in a form to search by zipcode", () => {
+    cy.get("form")
+      .get('input[name="location"]')
+      .type("31601")
+      .get('input[type="submit"]') 
+      .click();
+    cy.get(".animal-card")
+      .first()
+      .should("have.text", "🤍SadieSenior | Shih Tzu | Valdosta, GA");
+  });
+  it("should give an error message if there are no matching zipcodes", () => {
+    cy.get("form")
+      .get('input[name="location"]')
+      .type("34")
+      .get('input[type="submit"]')
+      .click();
+    cy.get(".zipcode-error").should("have.text", "Zip code should be 5 characters long.");
+  })
+});
